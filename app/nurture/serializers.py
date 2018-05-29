21d1616695799b6d982321f8812efdfc3b4a4185
Instaspiel-saurabh <saurabh@marketheed.com>
2018-05-29 12:49:00 +0530

    update nurture list API

--- app/nurture/serializers.py
+++ app/nurture/serializers.py
@@ -3,10 +3,17 @@ from app.nurture.models import Nurture,NurtureUrl
 
 
 class NurtureSerializer(serializers.ModelSerializer):
+	nutrure_url = serializers.SerializerMethodField("getNurtureUrl")
+	def getNurtureUrl(self, obj):
+		try:
+			return NurtureUrlSerializer(NurtureUrl.objects.filter(is_deleted= False,nurture=obj.id)[:5], many=True).data
+		except Exception as err :
+			print(err)
+			return None	
 	
 	class Meta:
 		model = Nurture
-		fields = ('id','name','company','description','is_deleted','created_at','updated_at')
+		fields = ('id','name','company','nutrure_url','description','is_deleted','created_at','updated_at')
 		extra_kwargs = {
 			'name': {
 				'required':True,
